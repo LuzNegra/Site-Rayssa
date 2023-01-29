b83c12c1c048ae33afd969c8c90426d74776012a
João Henrique <joaomoreira.passos@hotmail.com>
2023-01-29 10:11:16 -0300

    commit da função de data

---
+++ index.js
@@ -0,0 +1,170 @@
+const date = new Date();
+const dia = date.getDate();
+const mes = date.getMonth() + 1;
+const message = document.getElementById("message");
+var text_message;
+
+function Mudar_Texto() {
+    message.innerHTML = "<p> \n" +
+        text_message +
+        "</p>"
+}
+
+switch (mes) {
+    case 1: //Janeiro
+        switch (dia) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 15:
+                break;
+            case 16:
+                break;
+            case 17:
+                break;
+            case 18:
+                break;
+            case 19:
+                break;
+            case 20:
+                break;
+            case 21:
+                break;
+            case 22:
+                break;
+            case 23:
+                break;
+            case 24:
+                break;
+            case 25:
+                break;
+            case 26:
+                break;
+            case 27:
+                break;
+            case 28:
+                break;
+            case 29:
+                text_message = "Eu amo absolutamente tudo em vc sua beleza, sua inteligência, seu caráter, sua personalidade e até as suas manias. Eu não poderia pedir por uma pessoa mais especial para compartilhar a minha vida. Parabéns pelo seu dia, meu amor ❣️❣️❣️😘😘😘<br><br>Te desejo tudo de bom que tem nesse mundo!!!"
+                break;
+            case 30:
+                break;
+            case 31:
+                break;
+        }
+        break;
+    case 2: //Fevereiro
+        switch (dia) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 15:
+                break;
+            case 16:
+                break;
+            case 17:
+                break;
+            case 18:
+                break;
+            case 19:
+                break;
+            case 20:
+                break;
+            case 21:
+                break;
+            case 22:
+                break;
+            case 23:
+                break;
+            case 24:
+                break;
+            case 25:
+                break;
+            case 26:
+                break;
+            case 27:
+                break;
+            case 28:
+                break;
+            case 29:
+                break;
+            case 30:
+                break;
+            case 31:
+                break;
+        }
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    case 7:
+        break;
+    case 8:
+        break;
+    case 9:
+        break;
+    case 10:
+        break;
+    case 11:
+        break;
+    case 12:
+        break;
+    default:
+        text_message = "Você não possui mais mensagens"
+        break;
+}
